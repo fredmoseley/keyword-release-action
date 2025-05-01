d1c38e7881a93c11db9856925c0cc8e417fd40f7
fmoseley <fredmoseley@stanfordalumni.org>
2025-05-01 13:22:05 -0700

    Fixes HTTP status comparison in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,7 +40,7 @@ then
         echo "RESPONSE: $RESPSONSE"
         HTTP_STATUS=$(echo "$RESPONSE" | jq -r '.status')
 
-        if [ "$HTTP_STATUS" -ne 201 ]; then
+        if [ "$HTTP_STATUS" -ne "201" ]; then
             echo "Error: Failed to create the release. HTTP Status: $HTTP_STATUS"
             exit 1
         else
