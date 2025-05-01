953ba0a179f77718bd70b351e85bbc9a3ee58bb3
fmoseley <fredmoseley@stanfordalumni.org>
2025-05-01 12:09:53 -0700

    Fixes issue with response handling in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ then
     else
         echo "URL: $URL"
         echo "DATA: $DATA"
-        RESPSONSE=$DATA | http --verbose -A bearer -a ${GITHUB_TOKEN} POST $URL
+        RESPSONSE=$(echo "$DATA" | http --verbose -A bearer -a ${GITHUB_TOKEN} POST $URL)
 
         echo "RESPONSE: $RESPSONSE"
         if [ $? -ne 0 ]; then
